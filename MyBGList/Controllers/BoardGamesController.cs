86a09c37d6a513f206da2f19d683422a776e24dc
Aliuser11 <dajmispokojAZ@gmail.com>
2024-01-21 10:57:38 +0100

    Filtering;

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -67,16 +67,61 @@ namespace MyBGList.Controllers
         //        }
         //    };
         //}
+
         /* dynamically sort our records using the DBMS engine */
+        //[HttpGet(Name = "GetBoardGames")]
+        //[ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)] //set up a public cache with a max-age of 60 seconds for that cache  response
+        //public async Task <RestDTO<BoardGame[]>> Get(
+        //    int pageIndex = 0,
+        //    int pageSize = 10,//set a default value to the PageIndex and PageSize parameters(respectively, 0 and 10):
+        //    string sortColumn = "Name",
+        //    string sortOrder = "ASC")
+
+        //{
+        //    var query = _context.BoardGames
+        //        .OrderBy($"{sortColumn} {sortOrder}") // musi być spacja! OrderBy() extension method provided by Dynamic LINQ.
+        //        .Skip(pageIndex * pageSize)
+        //        .Take(pageSize);
+
+        //    return new RestDTO<BoardGame[]>()
+        //    {
+        //        Data = await query.ToArrayAsync(),
+        //        PageIndex = pageIndex, //Add the pageIndex and pageSize input parameters,
+        //        PageSize = pageSize,
+        //        RecordCount = await _context.BoardGames.CountAsync(), //Calculate the RecordCount
+
+        //        Links = new List<LinkDTO> {
+        //            new LinkDTO(
+        //                Url.Action(null, "BoardGames", new {pageIndex, pageSize }, Request.Scheme)!,
+        //                "self",
+        //                "GET"),
+        //            //example https://localhost:40443/BoardGames?sortColumn=Year - to retrieve the first 10 records, sorted by Year (ascending)
+        //        }
+        //    };
+        //}
+
+        /* Method with paging, sorting, and new => filtering */
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)] //set up a public cache with a max-age of 60 seconds for that cache  response
         public async Task <RestDTO<BoardGame[]>> Get(
             int pageIndex = 0,
             int pageSize = 10,//set a default value to the PageIndex and PageSize parameters(respectively, 0 and 10):
-            string sortColumn = "Name",
-            string sortOrder = "ASC")
+            string? sortColumn = "Name",
+            string? sortOrder = "ASC",
+            string? filterQuery = null)
+
         {
-            var query = _context.BoardGames
+            var query = _context.BoardGames.AsQueryable();
+            /*
+             It's also worth noting that we had to use the AsQueryable() method to
+            explicitly cast the DbSet<BoardGame> to an IQueryable<BoardGame>
+            interface, so that we could "chain" the extension methods to build the
+            expression tree
+             */
+            if (!string.IsNullOrEmpty(filterQuery))
+                query = query.Where(b => b.Name.Contains(filterQuery));
+            var recordCount = await query.CountAsync();
+            query = query     
                 .OrderBy($"{sortColumn} {sortOrder}") // musi być spacja! OrderBy() extension method provided by Dynamic LINQ.
                 .Skip(pageIndex * pageSize)
                 .Take(pageSize);
